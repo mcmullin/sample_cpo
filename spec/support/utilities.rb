0dc99536d4864e3854ddeba415f0beb281ff2bdd
Robert McMullin <robert.mcmullin@gmail.com>
2013-02-18 21:47:22 -0500

    Finish user edit, update, index, and destroy actions

--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,6 +1,15 @@
 include ApplicationHelper
 
-# see 8.3.3
+def sign_in(user)
+  visit signin_path
+  fill_in "Email",    with: user.email
+  fill_in "Password", with: user.password
+  click_button "Sign in"
+  # Sign in when not using Capybara as well.
+  cookies[:remember_token] = user.remember_token
+end
+
+# See 8.3.3
 =begin
 
 def valid_signin(user)
