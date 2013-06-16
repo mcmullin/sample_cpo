6f029f00e85f83f5388035041cd266c8c65adfb3
Robert McMullin <robert.mcmullin@gmail.com>
2013-06-15 22:51:36 -0400

    finish modeling products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -7,7 +7,7 @@ class ProductsController < ApplicationController
 	  respond_to do |format|
 	    format.html
 	    format.csv { send_data @products.to_csv }
-	    format.xls # { send_data @products.to_csv(col_sep: "\t") }
+	    format.xls #{ send_data @products.to_csv(col_sep: "\t") }
 	  end
 	end
 
